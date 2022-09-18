65ac14901126b4958621a39af9bdf966a80325f0
Abdur Rahman IsHa <74505859+abdurrahmanisha1@users.noreply.github.com>
2022-09-19 02:36:27 +0600

    clock

--- script.js
+++ script.js
@@ -6,10 +6,27 @@ window.addEventListener('scroll', function () {
 
 
 // Menu toggle 
-function toggleMenu(){
+function toggleMenu() {
     const menuToggle = document.querySelector('.menuToggle');
     const navigation = document.querySelector('.navigation');
     menuToggle.classList.toggle('active');
     navigation.classList.toggle('active');
 }
 
+
+// Watch 
+const deg = 6;
+const hr = document.querySelector('#hr');
+const mn = document.querySelector('#mn');
+const sc = document.querySelector('#sc');
+
+setInterval(() => {  //------1
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hr.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`
+    mn.style.transform = `rotateZ(${mm}deg)`
+    sc.style.transform = `rotateZ(${ss}deg)`
+});
